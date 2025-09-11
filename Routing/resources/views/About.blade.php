15793b0cad678f1449f42557390526688a62a774
vijay bahadur singh mehra <vijaybahadursinghmehra@gmail.com>
2025-09-11 11:31:39 +0530

    basic routing

---
+++ Routing/resources/views/About.blade.php
@@ -0,0 +1,9 @@
+<h1>this is basic routing program</h1>
+<br/>
+<h2>now you present on about page</h2>
+<br/>
+<a href="/">home section</a>
+<br/>
+<br/>
+<a href="contect">contect section</a>
+<br/>
