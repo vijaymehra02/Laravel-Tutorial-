984008ba9eeaa5cf630915b565c62bb12c3347ad
vijay bahadur singh mehra <vijaybahadursinghmehra@gmail.com>
2025-09-12 17:40:59 +0530

    add checkbox , radio buttun , select dropdown , range input frad in form

--- Form/app/Http/Controllers/usercontroller.php
+++ Form/app/Http/Controllers/usercontroller.php
@@ -9,8 +9,18 @@ class usercontroller extends Controller
    function adduser(Request $req){
     echo "user name  is $req->username";
     echo "<br/>";
-    echo "user email  is $req->useruseremail";
+    echo "user email  is $req->useremail";
     echo "<br/>";
-    echo "user city  is $req->usercity";
+    echo "user city  is $req->useregucation";
+    echo "<br/>";
+    echo "user skills is  ";
+    print_r($req->skill);
+    echo "<br/>";
+    echo "user gender  is $req->gender";
+    echo "<br/>";
+    echo "user city  is $req->city";
+    echo "<br/>";
+    echo "user fluent in english under 10  is $req->eng";
+    echo "<br/>";
    }
 }
--- Form/resources/views/user-form.blade.php
+++ Form/resources/views/user-form.blade.php
@@ -9,7 +9,36 @@
         <input placeholder="enter user email" name="useremail" type="text">
     </div>
     <div class="input-wrapper">
-        <input placeholder="enter user city" name="usercity" type="text">
+        <input placeholder="enter your highr egucation" name="useregucation" type="text">
+    </div>
+    <div class="input-wrapper">
+        <p>your skills</p>
+        <input type="checkbox" name="skill" value="php" id="checkphp">
+        <label for="checkphp">PHP</label>
+        <input type="checkbox" name="skill" value="node" id="checknode">
+        <label for="checkphp">node</label>
+        <input type="checkbox" name="skill" value="java" id="checkjava">
+        <label for="checkphp">java</label>
+    </div>
+    <div class="input-wrapper">
+        <p>gender</p>
+        <input type="radio" name="gender" value="male" id="gender1">
+        <label for="gender1">mail</label>
+        <input type="radio" name="gender" value="femail" id="gender2">
+        <label for="gender2">femail</label>
+    </div>
+    <div class="input-wrapper">
+        <p>select city</p>
+         <select name="city">
+            <option value="jbp">jabalpur</option>
+            <option value="bpl">bhopal</option>
+            <option value="idr">indore</option>
+            <option value="nsp">narsingpur</option>
+         </select>
+    </div>
+    <div class="input-wrapper">
+        <p>fluency in english</p>
+         <input type="range" min="0" max="10" name="eng" >
     </div>
     <div class="input-wrapper">
         <button>Add new user</button>
