7bc428f8b75cc57deb0d3e24894b9f4148aa0641
vijay bahadur singh mehra <vijaybahadursinghmehra@gmail.com>
2025-09-27 14:55:15 +0530

    insert data in mysql

---
+++ Mysql-Operations/app/Http/Controllers/StudentController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\studentt;
+
+class StudentController extends Controller
+{
+    function add(Request $request){
+        $student = new Studentt;
+        $student->name = $request->input('name');
+        $student->email = $request->input('email');
+        $student->phone = $request->input('phoneno');
+        $student->save();
+        if($student){
+            return "student added successfully";
+        }else{
+            return "NO student added" ;
+            
+        }
+
+   }
+}
---
+++ Mysql-Operations/resources/views/welcome.blade.php
@@ -0,0 +1,3 @@
+<h1>Welcome to form operation laravel tutorial</h1>
+
+<a href="add" >Add Student</a>
