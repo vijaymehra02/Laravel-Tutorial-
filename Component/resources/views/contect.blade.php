21f9f391ba11967df4775ab528f218bf6328c28c
vijay bahadur singh mehra <vijaybahadursinghmehra@gmail.com>
2025-10-04 12:24:44 +0530

    building layout with component

---
+++ Component/resources/views/contect.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <x-slot name="tital">contect page</x-slot>
+    <x-slot name="main">this is contect page </x-slot>
+    <x-slot name="footer">footer for contect page</x-slot>
+</x-layout>
---
+++ Component/resources/views/welcome.blade.php
@@ -0,0 +1,3 @@
+<h1>welcome in the component layout code</h1>
+<br/>
+<a href="home" >Home</a>
