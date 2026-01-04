2db6faa2d8748d5e525f12159a89dc8cb2791428
vijay bahadur singh mehra <vijaybahadursinghmehra@gmail.com>
2026-01-04 14:48:34 +0530

    send mail

---
+++ sendmail/app/Mail/welcomemail.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class welcomemail extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $msg;
+    public $sub;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($msg , $subject)
+    {
+        $this->msg = $msg;
+        $this->sub = $subject;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: $this->sub,
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'mail',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
